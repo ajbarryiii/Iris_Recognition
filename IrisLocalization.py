277ef8f495fd68070fbdce744c1ce6fb53f073d2
ajbarryiii <ajbarryiii@gmail.com>
2023-11-03 19:38:39 -0400

    Update IrisLocalization.py

--- IrisLocalization.py
+++ IrisLocalization.py
@@ -1,48 +1,87 @@
 import cv2
 import numpy as np
 
-def iris_localization(image_path):
-    # Read the image
-    img = cv2.imread(image_path, 0)
+def Iris_localize(img):
+  
+  # Compute the histogram of the image
+  histogram_x = np.sum(img, axis=0)
+  Xp = np.argmin(histogram_x)
 
-    # Step 1: Project the image in the vertical and horizontal direction
-    vertical_projection = np.sum(img, axis=1)
-    horizontal_projection = np.sum(img, axis=0)
+  histogram_x = np.sum(img, axis=0)
+  Xp = np.argmin(histogram_x)
 
-    # Find the approximate pupil center coordinates (Xp, Yp)
-    Yp = np.argmin(vertical_projection)
-    Xp = np.argmin(horizontal_projection)
+  histogram_y = np.sum(img, axis=1)
+  Yp = np.argmin(histogram_y)
 
-    # Step 2: Binarize a 120x120 region centered at (Xp, Yp)
-    half_size = 60
-    roi = img[Yp - half_size:Yp + half_size, Xp - half_size:Xp + half_size]
-    _, binary_roi = cv2.threshold(roi, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+  # Given Xp, Yp from previous computation
+  half_size = 60
+  roi = img[Yp - half_size:Yp + half_size, Xp - half_size:Xp + half_size]
 
-    # Find the centroid of the binary region
-    M = cv2.moments(binary_roi)
-    if M["m00"] != 0:
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
-    else:
-        cX, cY = half_size, half_size  # set centroid to the center of the ROI
+  # Adaptive thresholding
+  binary_roi = cv2.adaptiveThreshold(roi, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
 
-    # Correct the centroid to the original image scale
-    cX += Xp - half_size
-    cY += Yp - half_size
+  # Calculate the centroid
+  M = cv2.moments(binary_roi)
+  if M["m00"] != 0:
+    cY = int(M["m10"] / M["m00"])
+    cX = int(M["m01"] / M["m00"])
+  else:
+    cX, cY = 0, 0
 
-    # Step 3: Calculate exact parameters using edge detection and Hough transform
-    edges = cv2.Canny(img, 100, 200)
-    circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 120, param1=50, param2=30, minRadius=0, maxRadius=0)
+  # Updating pupil coordinates based on centroid
+  Xp_new = Xp - half_size + cX
+  Yp_new = Yp - half_size + cY
+  #Use a threshhold of 64 to localize the pupil
+  region120 = img[Yp_new-60:Yp_new+60,Xp_new-60:Xp_new+60]
+  ret,th1 = cv2.threshold(region120,64,65,cv2.THRESH_BINARY)
 
-    if circles is not None:
-        circles = np.uint16(np.around(circles))
-        for i in circles[0,:]:
-            cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
-            cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
+  #Based on the binary image, re-calculate the center of the pupil and estimate
+  # the radius of the pupil
+  mask1 = np.where(th1>0,1,0)
 
-    # Show the result
-    cv2.imshow('Iris Localization', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+  vertical = mask1.sum(axis = 0)
+  horizontal = mask1.sum(axis = 1)
 
-iris_localization("/Users/ajbarry/Dropbox/Documents/MA y2/Applied CV/Iris Recognition/CASIA Iris Image Database (version 1.0)/001/1/001_1_1.bmp")
+  minyp = np.argmin(horizontal) 
+  minxp = np.argmin(vertical)
+  radius1 = (120 - sum(mask1[minyp])) / 2
+  radius2 = (120 - np.sum(mask1,axis=0)[minxp]) / 2
+  radius = int((radius1 + radius2) /2)
+
+  region240 = img[np.arange(Yp_new-120, min(279, Yp_new+110)),:][:,np.arange(Xp_new-135,min(319,Xp_new+135))]
+  region120 = img[np.arange(Yp_new-60, min(279, Yp_new+60)),:][:,np.arange(Xp_new-60,min(319,Xp_new+60))]
+
+
+  for loop in range(1,5):
+      circles = cv2.HoughCircles(region120,cv2.HOUGH_GRADIENT,1,250, param1=50,param2=10,minRadius=(radius-loop),maxRadius=(radius+loop))
+      if type(circles) != type(None):
+          break
+      else:
+          pass
+  circles = np.around(circles)
+
+  region240_blur = cv2.GaussianBlur(region240, (5, 5), 0)
+  circles1 = cv2.HoughCircles(region240_blur, cv2.HOUGH_GRADIENT,1,250, param1=30,param2=10,minRadius=98,maxRadius=118)
+  circles1 = np.around(circles1)                           
+
+
+  image1 = img.copy()
+
+  for i in circles[0,:]:
+  # draw the outer circle
+    cv2.circle(image1,( int(i[0]+ Xp_new - 60),int(i[1] + Yp_new - 60)),int(i[2]),(0,255,0),2)
+# draw the center of the circle
+    cv2.circle(image1,( int(i[0]+ Xp_new - 60),int(i[1] + Yp_new - 60)),int(i[2]),(0,255,0),2)
+    innerCircle = [i[0] + Xp_new - 60,i[1] + Yp_new -60 ,i[2]]
+
+
+  for i in circles1[0,:]:
+  # draw the outer circle
+    cv2.circle(image1,( int(i[0]+ Xp_new - 135),int(i[1] + Yp_new - 120)),int(i[2]),(0,255,0),2)
+# draw the center of the circle
+    cv2.circle(image1,( int(i[0]+ Xp_new - 135),int(i[1] + Yp_new - 120)),int(i[2]),(0,255,0),3)
+    outterCircle = [int(i[0]+ Xp_new - 135),int(i[1] + Yp_new - 120),i[2]   ]
+  
+  return(innerCircle,outterCircle)
+
+
