077634010bfeb5f93957b2a0d3e54c192a528521
ajbarryiii <ajbarryiii@gmail.com>
2023-11-06 16:22:56 -0500

    Update IrisPerformanceEvaluation.py

--- IrisPerformanceEvaluation.py
+++ IrisPerformanceEvaluation.py
@@ -14,29 +14,28 @@ from IrisFeatureExtraction import *
 from IrisMatching import *
 
 
-def table_CRR(self, train_features, train_classes, test_features, test_classes):
-    # Assuming getMatching takes the features and classes as input
+def table_CRR(train_features, train_classes, test_features, test_classes):
+    # Assuming IrisMatching takes the features and classes as input
     # This method should generate a table of CRR based on different similarity measures
     vec = []
     dimensions = [100, 107]
     for i in range(1, 4):
         print(f'Currently computing distance measure number {i}')
         for dim in dimensions:
-            vec.append(getMatching(train_features, test_features, LDA_dimension=dim, distance_measure=i))
+            vec.append(IrisMatching(train_features, test_features, LDA_dimension=dim, distance_measure=i))
     vec = np.array(vec).reshape(3, 2)
     df = pd.DataFrame(vec, index=['L1 distance measure', 'L2 distance measure', 'Cosine similarity measure'],
                         columns=['Original Feature Set', 'Reduced Feature Set'])
-    print(df)
     return df
 
-def performance_evaluation(self, train_features, train_classes, test_features, test_classes):
+def performance_evaluation( train_features, train_classes, test_features, test_classes):
     # This method should plot the CRR curve for different PCA dimensions and LDA configurations
     vec = []
     dimensions = [50, 60, 70, 80, 90, 100, 107]
     plt.figure()
     for dim in dimensions:
         print(f'Currently computing dimension {dim}')
-        crr_value = getMatching(train_features, test_features, LDA_dimension=dim)
+        crr_value = IrisMatching(train_features, test_features, LDA_dimension=dim)
         vec.append(crr_value)
     lw = 2
     plt.plot(dimensions, vec, color='darkorange', lw=lw)
@@ -46,13 +45,28 @@ def performance_evaluation(self, train_features, train_classes, test_features, t
     plt.scatter(dimensions, vec, marker='*')
     plt.show()
 
-def FM_FNM_table(fmrs_mean,fmrs_l,fmrs_u,fnmrs_mean,fnmrs_l,fnmrs_u,thresholds):
-    print ("False Match and False Nonmatch Rates with Different Threshold Values")
-    print tabulate([[thresholds[7], str(fmrs_mean[7])+"["+str(fmrs_l[7])+","+str(fmrs_u[7])+"]",str(fnmrs_mean[7])+"["+str(fnmrs_l[7])+","+str(fnmrs_u[7])+"]"], 
-                    [thresholds[8], str(fmrs_mean[8])+"["+str(fmrs_l[8])+","+str(fmrs_u[8])+"]",str(fnmrs_mean[8])+"["+str(fnmrs_l[8])+","+str(fnmrs_u[8])+"]"],
-                    [thresholds[9], str(fmrs_mean[9])+"["+str(fmrs_l[9])+","+str(fmrs_u[9])+"]",str(fnmrs_mean[9])+"["+str(fnmrs_l[9])+","+str(fnmrs_u[9])+"]"]],
-                   headers=['Threshold', 'False match rate(%)',"False non-match rate(%)"])
-#FM_FNM_table(train_features, train_classes, test_features, test_classes, thresholds_2)
+def FM_FNM_table(fmrs_mean, fmrs_l, fmrs_u, fnmrs_mean, fnmrs_l, fnmrs_u, thresholds):
+    
+    print("False Match and False Nonmatch Rates with Different Threshold Values")
+    # Prepare data in a format suitable for DataFrame
+    data = {
+        'Threshold': thresholds[7:10],
+        'False match rate(%)': [
+            f"{fmrs_mean[i]}[{fmrs_l[i]},{fmrs_u[i]}]" for i in range(7, 10)
+        ],
+        "False non-match rate(%)": [
+            f"{fnmrs_mean[i]}[{fnmrs_l[i]},{fnmrs_u[i]}]" for i in range(7, 10)
+        ]
+    }
+    
+    # Create DataFrame
+    df = pd.DataFrame(data)
+    
+    # Set the display.max_colwidth to -1 to show all text in a column
+    pd.set_option('display.max_colwidth', None)
+    
+    # Print DataFrame without the index
+    print(df.to_string(index=False))
 
 def FMR_conf(fmrs_mean,fmrs_l,fmrs_u,fnmrs_mean,fnmrs_l,fnmrs_u):
     plt.figure()
